59037efc9345679a6ca4721b6e7d30ab581d1758
Rich Moyse <rich@moyse.us>
2015-05-12 00:02:36 -0400

    Supplant screen with tmux but leave screen available.

--- scriptInstall/versionSpecifiers.sh
+++ scriptInstall/versionSpecifiers.sh
@@ -2,4 +2,4 @@
 VERSION_apt_transport_https='=0.8.16~exp12ubuntu10.21'
 VERSION_lxc_docker='-1.3.3'
 VERSION_make='=3.81-8.1ubuntu1.1'
-VERSION_screen='=4.0.3-14ubuntu8'
+VERSION_tmux='=1.9a-1~ppa1~p'
