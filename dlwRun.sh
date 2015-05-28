b06b9b30dfec21bc54c40962638f0e3e5b38c8b1
Rich Moyse <rich@moyse.us>
2015-05-27 23:35:13 -0400

    Added ability to specify tmux.conf file and implemented getopts parsing.

--- dlwRun.sh
+++ dlwRun.sh
@@ -2,20 +2,14 @@
 ###############################################################################
 #
 #  Purpose: 
-#    Download and install the image to the Docker Daemon's local repository.
+#    Download and install the desired 'dlw' image to the Docker Daemon's
+#    local repository and bind host project and tmux configuration directories
+#    to container when desired.
 #
 #  Assumes:
 #    1.  Local Docker Daemon accessible via its usual socket.
 #    2.  Created container tty will attach to current CLI terminal.
 #    
-#  Input:
-#    $1 - dlw image tag name of desired dlw version.
-#    $2 - Optional Daemon host directory that will 
-#
-#  Output:
-#    When failure:
-#      Either the error code from failed install process or rm failure code
-#
 ###############################################################################
 function helpText () {
 cat <<COMMAND_HELP
@@ -23,14 +17,21 @@ cat <<COMMAND_HELP
 Pull, create and run a new dlw container and connect it to Project files located
 on the current host's file system. Pull occurs iff dlw isn't cached.
 
-Usage: dlwRun.sh <HubTagName> [<ProjectDirectory>]
+Usage: dlwRun.sh [OPTIONS] [HUB_TAG_NAME]
 
+
+    HUB_TAG_NAME   Docker Hub Tag associated to desired dlw image.
+                   dlw tag name concatenates: <dlw-version>_<DockerDaemon-version>.
+                   If omitted, tag name assigned 'latest_latest'
+                   see: https://registry.hub.docker.com/u/whisperingchaos/dlw/tags/manage/
+                    
 OPTIONS:
-    <HubTagName>        Docker Hub Tag associated to desired dlw image.
-                        dlw tag name concatenates: <dlw-version>_<DockerDaemon-version>
-                        see: https://registry.hub.docker.com/u/whisperingchaos/dlw/tags/manage/
-    <ProjectDirectory>  Existing host directory containing one or more
-                        dlw Projects as subdirectories.
+
+    -p             Existing host directory containing one or more dlw Projects as
+                   subdirectories.  If omitted, Projects are stored in container.
+
+    -t             Existing host directory containing the '.tmux.conf' file. If omitted
+                   tmux provides its own default settings.
 
 For more help: https://github.com/WhisperingChaos/DockerLocalWorkbench#ToC
 
@@ -50,8 +51,44 @@ cat <<HOST_DIR_WARN
 HOST_DIR_WARN
 }
 ###############################################################################
-if [ -z "$1" ] || [ "$1" == "--help" ]; then helpText; exit 0; fi
-if [ -z "$2" ]; then
+OPTIND=1
+unset HOST_PROJECT_DIR
+unset HOST_TMUX_DIR
+while getopts 'h?p:t:' opt; do
+    case "$opt" in
+    h|\?)
+        helpText
+        exit 0
+        ;;
+    p)  declare HOST_PROJECT_DIR="$OPTARG"
+        if ! [ -d "$HOST_PROJECT_DIR" ]; then
+          echo "Abort: -p option doesn't resolve to an existing host directory: '$HOST_PROJECT_DIR'.">&2
+          exit 1
+        fi
+        HOST_PROJECT_DIR="-v $HOST_PROJECT_DIR:/home/dlw/project"
+        ;;
+    t)  declare HOST_TMUX_DIR="$OPTARG"
+        if ! [ -d "$HOST_TMUX_DIR" ]; then
+          echo "Abort: -t option doesn't resolve to an existing host directory: '$HOST_TMUX_DIR'.">&2
+          exit 1
+        fi
+        if ! [ -e "$HOST_TMUX_DIR/.tmux.conf" ]; then
+          echo "Abort: -t option host directory: '$HOST_TMUX_DIR', doesn't contain: '.tmux.conf'.">&2
+          exit 1
+        fi
+        HOST_TMUX_DIR="-v $HOST_TMUX_DIR:/home/dlw/.tmuxconfdir"
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+declare DLW_TAG='latest_latest'
+if [ -n "$1" ]; then
+  DLW_TAG="$1"
+fi
+
+if [ -z "$HOST_PROJECT_DIR" ]; then
   hostDirWarningText
   read -p "Confirm: Project files are local to the started container (Y/n):" -N 1 -- Yn
   if [ "$Yn" != 'Y' ]; then
@@ -60,12 +97,6 @@ if [ -z "$2" ]; then
     exit 1;
   fi
   echo
-  unset HOST_PROJECT_DIR
-elif ! [ -d "$2" ]; then
-  echo "Abort: Second argument doesn't resolve to an existing host directory: '$2'.">&2
-  exit 1;
-else
-  HOST_PROJECT_DIR="-v $2:/home/dlw/project"
 fi
-# create a container to run dlw.  Container will attach to command line of current terminal's
-docker run -i -t -v /var/run/docker.sock:/var/run/docker.sock $HOST_PROJECT_DIR -- whisperingchaos/dlw:$1
+# create a container to run dlw.  Container will attach to current terminal
+docker run -i -t -v /var/run/docker.sock:/var/run/docker.sock $HOST_PROJECT_DIR $HOST_TMUX_DIR -- dlw:$DLW_TAG # whisperingchaos/dlw:$DLW_TAG
