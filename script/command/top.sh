ce30aba58da98bd8daa949f8e462d77a4cfed455
Rich Moyse <rich@moyse.us>
2015-06-07 22:37:24 -0400

    Added in container specific code as layer before reporting interface layer. Removed references to ps command.  Removed the help & argument sections as they are included by the reporting layer.

--- script/command/top.sh
+++ script/command/top.sh
@@ -6,13 +6,14 @@ source "ComponentListVerifyInclude.sh";
 source "ArgumentsDockerVerifyInclude.sh";
 source "VirtCmmdInterface.sh";
 source "VirtDockerInterface.sh";
+source "VirtDockerContainerInterface.sh";
 source "VirtDockerReportingInterface.sh";
 source "VirtDockerReportingCmmdMultiInterface.sh";
 source "PacketInclude.sh";
 ##############################################################################
 ##
 ##  Purpose:
-##    Configure container virtual functions to implement 'kill' command.
+##    Configure container virtual functions to implement 'top' command.
 ##
 #################################################################################
 ##
@@ -20,42 +21,13 @@ source "PacketInclude.sh";
 function VirtDockerReportingCommandNameGet () {
   echo 'top'
 }
-###############################################################################
-##
-##  Purpose:
-##    Describes purpose and arguments for reporting commands.
-##
-###############################################################################
-function VirtCmmdHelpDisplay () {
-local -r commandName="`VirtDockerReportingCommandNameGet`"
-ComponentNmListArgument "$commandName" 'all'
-
-cat <<COMMAND_HELP_Purpose
-
-Report on targeted Components' container(s).  Wraps docker '$commandName' command.
-
-Usage: dlw $commandName [OPTIONS] TARGET 
-
-COMMAND_HELP_Purpose
-  HelpCommandTarget
-  HelpOptionHeading
-  HelpComponentVersion 'cur'
-  HelpIgnoreStateDocker 'false'
-  HelpNoExecuteDocker 'false'
-  HelpShowDocker 'false'
-  HelpColumnSelectExclude 'ComponentName/COMPONENT/15,ContainerGUID/CONTAINER ID/12'
-  HelpColumnHeadingRemove 'false'
-  HelpHelpDisplay 'false'
-  DockerOptionsFormat "$commandName"
-  function psOptionsFormat () {
-    function VirtOptionsExtractHelpDisplay () {
-      ps --help
-    }
-    OptionsExtract 'ps'
-  }
-  psOptionsFormat
+function VirtDockerContainerCommandNameGet () {
+  VirtDockerReportingCommandNameGet
 }
-##############################################################################
+function VirtDockerContainerOrderingGet () {
+  # order of prerequsites as they are, not in reverse
+  echo 'true'
+}
 function VirtDockerReportingHeadingRegex () {
   echo '^UID|LABEL\ *PID\ '
   return 0
